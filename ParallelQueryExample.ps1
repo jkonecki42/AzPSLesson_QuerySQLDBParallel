c967ce347af06c17a49bf5cc049acea3169a3927
JoshK <joshkonecki@gmail.com>
2024-06-12 15:09:50 -0400

    Added script.

---
+++ ParallelQueryExample.ps1
@@ -0,0 +1,11 @@
+<#
+  Powershell 7.4.2     https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.4
+  Az 12.0.0            Install-Module Az -Scope CurrentUser
+  SQLServer 22.2.0     Install-Module Az -Scope CurrentUser
+  Az.Sql Documentation https://learn.microsoft.com/en-us/powershell/module/az.sql/?view=azps-12.0.0
+#>
+
+
+#Connect-AzAccount -SubscriptionName 'Dev-Test 1'
+
+
